e77f24924620e7373db01064ec98e7e1771647dd
Carlos Salas <carlos.salas@suse.com>
2023-10-19 11:10:22 +0200

    chore: encrypt gh actions secret outputs
    Signed-off-by: Carlos Salas <carlos.salas@suse.com>

--- scripts/image-digest.sh
+++ scripts/image-digest.sh
@@ -2,6 +2,7 @@
 
 # Run your command and capture its output
 output=$(make docker-list-all REGISTRY="$1" ORG="$2" TAG="$3")
+PASSPHRASE="$4"
 
 # Use a for loop to iterate over each line
 IFS=$'\n'       # Set the Internal Field Separator to newline
@@ -14,9 +15,18 @@ for line in $output; do
   # Run the Docker command and get the digest
   digest=$(docker buildx imagetools inspect "$line" --format '{{json .}}' | jq -r .manifest.digest)
 
-  # Add image name and digest to the output
-  echo "${githubimageoutput[$line_count]}=$line" >> "$GITHUB_OUTPUT"
-  echo "${githubdigestoutput[$line_count]}=$digest" >> "$GITHUB_OUTPUT"
+  # Add encrypted image name to the output
+  image_output="$line"
+  #encrypted_image=$(gpg --symmetric --batch --passphrase ${PASSPHRASE} --output - <(echo ${image_output}) | base64 -w0)
+  #echo "${githubimageoutput[$line_count]}=${encrypted_image}" >> "$GITHUB_OUTPUT"
+  echo "::add-mask::${image_output}"
+  echo "${githubimageoutput[$line_count]}=${image_output}"
+  # Add encrypted digest to the output
+  digest_output="$digest"
+  #encrypted_digest=$(gpg --symmetric --batch --passphrase ${PASSPHRASE} --output - <(echo ${digest_output}) | base64 -w0)
+  #echo "${githubdigestoutput[$line_count]}=${encrypted_digest}" >> "$GITHUB_OUTPUT"
+  echo "::add-mask::${digest_output}"
+  echo "${githubdigestoutput[$line_count]}=${digest_output}"
 
   # Increment the line counter
   line_count=$((line_count + 1))
